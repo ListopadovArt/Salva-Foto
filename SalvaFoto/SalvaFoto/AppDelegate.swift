648297d080389b2a081130fc3fd915484fdae785
Artem Listopadov <artbdarm9@mail.ru>
2022-07-31 23:04:24 +0300

    init: start project

---
+++ SalvaFoto/SalvaFoto/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SalvaFoto
+//
+//  Created by Artem Listopadov on 31.07.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .orange
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
---
+++ SalvaFoto/SalvaFoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalvaFoto
+//
+//  Created by Artem Listopadov on 31.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
