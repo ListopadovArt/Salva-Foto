0a79e9e0452c48ffce8be27de3896d4b533b5d23
Artem Listopadov <artbdarm9@mail.ru>
2022-08-24 14:41:45 +0300

    fix: Change the color of the like button

--- SalvaFoto/Buttons.swift
+++ SalvaFoto/Buttons.swift
@@ -11,6 +11,6 @@ import UIKit
 func makeButton(button: UIButton, scale: UIImage.SymbolScale = .large, systemName: String) {
     let configuration = UIImage.SymbolConfiguration(scale: scale)
     let saveImage = UIImage(systemName: systemName, withConfiguration: configuration)
-    button.tintColor = .white
+    button.tintColor = .appColor
     button.setImage(saveImage, for: .normal)
 }
