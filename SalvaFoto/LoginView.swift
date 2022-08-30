1da4a6afbb7adc8188e08ce105cf846f55c7b0bb
Artem Listopadov <artbdarm9@mail.ru>
2022-08-31 00:28:30 +0300

    feat: Add Login View

---
+++ SalvaFoto/LoginView.swift
@@ -0,0 +1,94 @@
+//
+//  LoginView.swift
+//  SalvaFoto
+//
+//  Created by Artem Listopadov on 30.08.22.
+//
+
+import UIKit
+
+class LoginView: UIView {
+    
+    let titleLabel = UILabel()
+    let signButton = UIButton(type: .system)
+    let errorMessageLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension LoginView {
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 5
+        clipsToBounds = true
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title2)
+        titleLabel.numberOfLines = 1
+        titleLabel.text = "Login to Unsplash"
+        titleLabel.textColor = .white
+        
+        signButton.translatesAutoresizingMaskIntoConstraints = false
+        signButton.configuration = .filled()
+        signButton.configuration?.imagePadding = 8
+        signButton.setTitle("Sign In", for: [])
+        signButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
+        signButton.tintColor = .appColor
+        signButton.setTitleColor(.black, for: .normal)
+        
+        errorMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorMessageLabel.textAlignment = .center
+        errorMessageLabel.textColor = .systemRed
+        errorMessageLabel.numberOfLines = 0
+        //        errorMessageLabel.isHidden = true
+        errorMessageLabel.text = "Error example..."
+        
+    }
+    
+    func layout() {
+        addSubview(titleLabel)
+        addSubview(signButton)
+        addSubview(errorMessageLabel)
+        
+        // Title
+        NSLayoutConstraint.activate([
+            signButton.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 5),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        
+        // SignButton
+        NSLayoutConstraint.activate([
+            signButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            signButton.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            signButton.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+        ])
+        
+        // ErrorLabel
+        NSLayoutConstraint.activate([
+            errorMessageLabel.topAnchor.constraint(equalToSystemSpacingBelow: signButton.bottomAnchor, multiplier: 3),
+            errorMessageLabel.leadingAnchor.constraint(equalTo: signButton.leadingAnchor),
+            errorMessageLabel.trailingAnchor.constraint(equalTo: signButton.trailingAnchor)
+        ])
+    }
+}
+
+extension LoginView {
+    @objc func signInTapped(sender: UIButton){
+        // errorMessageLabel.isHidden = true
+        login()
+    }
+    
+    private func login() {
+        print("Login")
+    }
+}
--- SalvaFoto/ProfileViewController.swift
+++ SalvaFoto/ProfileViewController.swift
@@ -8,7 +8,29 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    let loginView = LoginView()
+    
     override func viewDidLoad() {
-        view.backgroundColor = .systemPurple
+        style()
+        layout()
+    }
+}
+
+extension ProfileViewController {
+    private func style() {
+        self.view.backgroundColor = .backgroundColor
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        // LoginView
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginView.heightAnchor.constraint(equalToConstant: 300),
+            loginView.widthAnchor.constraint(equalToConstant: 200),
+        ])
     }
 }
