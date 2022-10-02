872ab030394c77b5dd2d4652599d967a731b0e7e
Artem Listopadov <artbdarm9@mail.ru>
2022-10-02 23:18:05 +0300

    feat: Implement Settings View Controller

--- SalvaFoto/SettingsViewController.swift
+++ SalvaFoto/SettingsViewController.swift
@@ -7,11 +7,19 @@
 
 import UIKit
 
+struct SettingsMenuModel {
+    var header: String?
+    var numberOfCell: Int
+}
+
 class SettingsViewController: UIViewController {
     
-    let stackView = UIStackView()
-    let label = UILabel()
+    var tableView = UITableView()
     
+    var tableModel: [SettingsMenuModel] = [SettingsMenuModel(header: "Support", numberOfCell: 1),
+                                           SettingsMenuModel(header: "Account", numberOfCell: 1),
+                                           SettingsMenuModel(header: "Other", numberOfCell: 3),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,30 +32,131 @@ extension SettingsViewController {
     func style() {
         self.view.backgroundColor = .backgroundColor
         
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 20
-        
-        // Label
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
-        label.font = UIFont.preferredFont(forTextStyle: .title3)
-        label.adjustsFontForContentSizeCategory = true
-        label.numberOfLines = 0
-        label.text = "Welcome"
-        label.textColor = .white
+        // Table
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.showsVerticalScrollIndicator = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.isScrollEnabled = false
+        tableView.backgroundColor = .backgroundColor
+        tableView.register(SettingsCell.self, forCellReuseIdentifier:  SettingsCell.reuseID)
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+        tableView.rowHeight = SettingsCell.rowHeight
     }
     
     func layout() {
-        stackView.addArrangedSubview(label)
-        
-        view.addSubview(stackView)
+        self.view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+}
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return tableModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return tableModel[section].header
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let rowsInSection = tableModel[section].numberOfCell
+        return rowsInSection
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.frame = CGRect(x: 32, y: 10, width: tableView.frame.size.width - 48, height: 28)
+        label.textColor = UIColor(red: 0.282, green: 0.282, blue: 0.29, alpha: 1)
+        label.text = tableModel[section].header
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        headerView.addSubview(label)
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsCell.reuseID, for: indexPath) as? SettingsCell else {
+            return UITableViewCell()
+        }
+        
+        switch indexPath.section {
+        case 0:
+            cell.titleLabel.text = "Give us Feedback"
+            cell.titleImage.image = UIImage(systemName: "message.fill")
+        case 1:
+            cell.titleLabel.text = "Edit Profile"
+            cell.titleImage.image = UIImage(systemName: "person.fill")
+        default:
+            switch indexPath.row {
+            case 0:
+                cell.titleLabel.text = "Privacy Policy"
+                cell.titleImage.image = UIImage(systemName: "doc.text.fill")
+            case 1:
+                cell.titleLabel.text = "Log out"
+                cell.titleImage.image = UIImage(systemName: "square.lefthalf.fill")
+            default:
+                cell.titleLabel.text = "Delete account"
+                cell.titleImage.image = UIImage(systemName: "trash.fill")
+            }
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //TODO: - Add action functionality
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if (cell.responds(to: #selector(getter: UIView.tintColor))){
+            let cornerRadius: CGFloat = 10.0
+            cell.backgroundColor = .clear
+            let layer: CAShapeLayer = CAShapeLayer()
+            let path: CGMutablePath = CGMutablePath()
+            let bounds: CGRect = cell.bounds.insetBy(dx: 16.0, dy: 0.0)
+            var addLine: Bool = false
+            if indexPath.row == 0 && indexPath.row == ( tableView.numberOfRows(inSection: indexPath.section) - 1) {
+                path.addRoundedRect(in: bounds, cornerWidth: cornerRadius, cornerHeight: cornerRadius)
+            } else if indexPath.row == 0 {
+                path.move(to: CGPoint(x: bounds.minX, y: bounds.maxY))
+                path.addArc(tangent1End: CGPoint(x: bounds.minX, y: bounds.minY), tangent2End: CGPoint(x: bounds.midX, y: bounds.minY), radius: cornerRadius)
+                path.addArc(tangent1End: CGPoint(x: bounds.maxX, y: bounds.minY), tangent2End: CGPoint(x: bounds.maxX, y: bounds.midY), radius: cornerRadius)
+                path.addLine(to: CGPoint(x: bounds.maxX, y: bounds.maxY))
+                addLine = true
+            } else if indexPath.row == (tableView.numberOfRows(inSection: indexPath.section) - 1) {
+                path.move(to: CGPoint(x: bounds.minX, y: bounds.minY))
+                path.addArc(tangent1End: CGPoint(x: bounds.minX, y: bounds.maxY), tangent2End: CGPoint(x: bounds.midX, y: bounds.maxY), radius: cornerRadius)
+                path.addArc(tangent1End: CGPoint(x: bounds.maxX, y: bounds.maxY), tangent2End: CGPoint(x: bounds.maxX, y: bounds.midY), radius: cornerRadius)
+                path.addLine(to: CGPoint(x: bounds.maxX, y: bounds.minY))
+            } else {
+                path.addRect(bounds)
+                addLine = true
+            }
+            layer.path = path
+            layer.fillColor = UIColor.backViewColor.cgColor
+            if addLine {
+                let lineLayer: CALayer = CALayer()
+                let lineHeight: CGFloat = 2.0 / UIScreen.main.scale
+                lineLayer.frame = CGRect(x: bounds.minX + 0.0, y: bounds.size.height - lineHeight, width: bounds.size.width, height: lineHeight)
+                lineLayer.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1).cgColor
+                layer.addSublayer(lineLayer)
+            }
+            let testView: UIView = UIView(frame: bounds)
+            testView.layer.insertSublayer(layer, at: 0)
+            testView.backgroundColor = .clear
+            cell.backgroundView = testView
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40.0
     }
 }
--- SalvaFoto/UIColor+Utils.swift
+++ SalvaFoto/UIColor+Utils.swift
@@ -10,5 +10,5 @@ import UIKit
 extension UIColor {
     static let appColor = UIColor(red: 0.86, green: 0.96, blue: 0.02, alpha: 1.00)
     static let backgroundColor = UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 1.00)
-    static let backViewColor = UIColor(red: 0.56, green: 0.56, blue: 0.60, alpha: 1.00)
+    static let backViewColor = UIColor(red: 0.282, green: 0.282, blue: 0.29, alpha: 1)
 }
