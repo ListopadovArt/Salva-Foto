3fddba2a218af30f9531fa8a5e2dfcb02af50fa1
Artem Listopadov <artbdarm9@mail.ru>
2022-11-21 22:15:31 +0300

    feat: Add LaunchScreen
    - Create LaunchScreen
    - Update App Icon

--- SalvaFoto/UIColor+Utils.swift
+++ SalvaFoto/UIColor+Utils.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIColor {
-    static let appColor = UIColor(red: 0, green: 0.659, blue: 0.475, alpha: 1)
+    static let appColor = UIColor(red: 0, green: 0.659, blue: 0.475, alpha: 1) // #00A879
     static let backgroundColor = UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 1.00)
     static let backViewColor = UIColor(red: 0.282, green: 0.282, blue: 0.29, alpha: 1)
 }
